2939ed7b1f48ee29c6361a9ee141f898120181fc
Snowflake-Anblicks <115771546+Snowflake-Anblicks@users.noreply.github.com>
2022-11-15 22:21:03 +0530

    Delete Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-connection_parameters = {
-    "account": "anblickspartner.east-us-2.azure",
-    "user": "mkumar",
-    "password": "Amumanu0729@",
-    "role": "ACCOUNTADMIN",
-    "warehouse": "DEV_WH",
-    "database": "CUSTOMERAI",
-    "schema": "MAIN"
-    }
